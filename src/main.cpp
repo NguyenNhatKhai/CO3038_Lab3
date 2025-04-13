136c42793f1645dd7b9f79f2c693bffad1b0bf2b
NguyenNhatKhai <khai.nguyen352528@hcmut.edu.vn>
2025-04-13 16:53:38 +0700

    ver10 final

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,22 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-constexpr uint32_t SERIAL_DEBUG_BAUD = 9600UL;
-
-constexpr char WIFI_SSID[] = "Oreki";
-constexpr char WIFI_PASSWORD[] = "hardware";
-
-constexpr char TOKEN[] = "zhNGUEL74XzDLd40IQKZ";
-constexpr char THINGSBOARD_SERVER[] = "app.coreiot.io";
-constexpr uint16_t THINGSBOARD_PORT = 1883U;
-constexpr uint16_t MAX_MESSAGE_SEND_SIZE = 512U;
-constexpr uint16_t MAX_MESSAGE_RECEIVE_SIZE = 512U;
-
 constexpr char CURRENT_FIRMWARE_TITLE[] = "DHT";
 constexpr char CURRENT_FIRMWARE_VERSION[] = "1.0";
 constexpr uint8_t FIRMWARE_FAILURE_RETRIES = 12U;
 constexpr uint16_t FIRMWARE_PACKET_SIZE = 4096U;
 constexpr uint8_t MAX_ATTRIBUTES = 1U;
+
+constexpr char WIFI_SSID[] = "Oreki";
+constexpr char WIFI_PASSWORD[] = "hardware";
+constexpr char TOKEN[] = "zhNGUEL74XzDLd40IQKZ";
+constexpr char THINGSBOARD_SERVER[] = "app.coreiot.io";
+
+constexpr uint16_t THINGSBOARD_PORT = 1883U;
+constexpr uint16_t MAX_MESSAGE_SEND_SIZE = 512U;
+constexpr uint16_t MAX_MESSAGE_RECEIVE_SIZE = 512U;
+constexpr uint32_t SERIAL_DEBUG_BAUD = 9600UL;
+constexpr uint64_t REQUEST_TIMEOUT_MICROSECONDS = 10000U * 1000U;
 
 Shared_Attribute_Update<1U, MAX_ATTRIBUTES> shared_update;
 Attribute_Request<2U, MAX_ATTRIBUTES> attr_request;
@@ -57,6 +57,23 @@ volatile double humidity = 0.0;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+void update_starting_callback() {}
+
+void finished_callback(const bool& success) {
+  if (success) {
+    Serial.println("Done, reboot now");
+    esp_restart();
+    return;
+  }
+  Serial.println("Downloading firmware failed");
+}
+
+void progress_callback(const size_t& current, const size_t& total) {
+  Serial.printf("Progress %.2f%%\n", static_cast<float>(current * 100U) / total);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void InitWiFi() {
   Serial.print("Connecting to WiFi ...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -80,13 +97,6 @@ void InitThingsBoard() {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void TaskHelloWorld(void *pvParameters) {
-  while (1) {
-    Serial.println("Hello World!");
-    vTaskDelay(1000);
-  }
-}
 
 void TaskWiFi(void *pvParameters) {
   while(1) {
@@ -121,6 +131,27 @@ void TaskDHT20(void *pvParameters) {
   }
 }
 
+void TaskOTA(void *pvParameters) {
+  while(1) {
+    if (!currentFWSent) {
+      currentFWSent = ota.Firmware_Send_Info(CURRENT_FIRMWARE_TITLE, CURRENT_FIRMWARE_VERSION);
+    }
+    if (!updateRequestSent) {
+      Serial.print(CURRENT_FIRMWARE_TITLE);
+      Serial.println(CURRENT_FIRMWARE_VERSION);
+      Serial.println("Firwmare update ...");
+      const OTA_Update_Callback callback(CURRENT_FIRMWARE_TITLE, CURRENT_FIRMWARE_VERSION, &updater, &finished_callback, &progress_callback, &update_starting_callback, FIRMWARE_FAILURE_RETRIES, FIRMWARE_PACKET_SIZE);
+      updateRequestSent = ota.Start_Firmware_Update(callback);
+      if(updateRequestSent) {
+        Serial.println("Firwmare update subscription...");
+        updateRequestSent = ota.Subscribe_Firmware_Update(callback);
+      }
+    }
+    thingsboard.loop();
+    vTaskDelay(1000);
+  }
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -132,10 +163,10 @@ void setup() {
   pinMode(GPIO_NUM_2, OUTPUT);
   InitWiFi();
   InitThingsBoard();
-  // xTaskCreate(TaskHelloWorld, "HelloWorld", 2048, NULL, 2, NULL);
   xTaskCreate(TaskWiFi, "WiFi", 2048, NULL, 2, NULL);
   xTaskCreate(TaskThingsBoard, "ThingsBoard", 2048, NULL, 2, NULL);
   xTaskCreate(TaskDHT20, "DHT20", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskOTA, "OTA", 4096, NULL, 2, NULL);
 }
 
 void loop() {}
